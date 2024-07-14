49548bd142436d534cc6b7e8f37b342ea4c467e1
shunsasaki0705 <shunsasaki0705@gmail.com>
2024-07-14 20:46:44 +0900

    modify sample.py for no argv

--- work/sample.py
+++ work/sample.py
@@ -2,8 +2,16 @@ import math
 import sys
 
 def main():
-  val = float(sys.argv[1])
-  print(math.radians(val))
+    if len(sys.argv) < 2:
+        print("Usage: python sample.py <number>")
+        sys.exit(1)
+
+    try:
+        val = float(sys.argv[1])
+        print(f"The provided value is {val}")
+    except ValueError:
+        print("Please provide a valid number.")
+        sys.exit(1)
 
 if __name__ == "__main__":
   main()
